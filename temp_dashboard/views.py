8cec7be6b21013bd0df867644a4810fc67338301
Nerdenator <a.mcruer@live.com>
2020-03-07 21:08:18 -0600

    can list all temperatures in template. need to get them into javascript somehow.

--- temp_dashboard/views.py
+++ temp_dashboard/views.py
@@ -6,4 +6,10 @@ from .models import Sensor, Temperature
 def dashboard(request):
     sensors = Sensor.objects.values()
     list_sensors = [sensor for sensor in sensors]
-    return render(request, 'temp_dashboard/index.html', {'sensors': list_sensors})
+    temps_dictionary = {}
+    for sensor in sensors:
+        temps = Temperature.objects.filter(sensor_id=sensor['id'])
+        temps_dictionary.update({sensor['location']: temps})
+
+    return render(request, 'temp_dashboard/index.html', {'sensors': list_sensors, 'temps_dictionary': temps_dictionary})
+
