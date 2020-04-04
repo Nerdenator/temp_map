4b0d42dd3843370239805dd7c3eb1ad3eb0bdcb3
Nerdenator <a.mcruer@live.com>
2020-04-04 00:03:04 -0500

    re-thinking front end.

--- temp_dashboard/views.py
+++ temp_dashboard/views.py
@@ -7,9 +7,12 @@ def dashboard(request):
     sensors = Sensor.objects.values()
     list_sensors = [sensor for sensor in sensors]
     temps_dictionary = {}
+    temp_celsius_values = []
     for sensor in sensors:
         temps = Temperature.objects.filter(sensor_id=sensor['id'])
         temps_dictionary.update({sensor['location']: temps})
+        for temp in temps:
+            temp_celsius_values.append(temp.celsius)
 
-    return render(request, 'temp_dashboard/index.html', {'sensors': list_sensors, 'temps_dictionary': temps_dictionary})
-
+    return render(request, 'temp_dashboard/index.html',
+                  {'sensors': list_sensors, 'temps_dictionary': temps_dictionary, 'temps_celsius': temp_celsius_values })
